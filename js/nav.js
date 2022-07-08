f64118add88b074274f323144c305797177ec5ef
zacharygawthorp <107508857+zacharygawthorp@users.noreply.github.com>
2022-07-08 11:37:26 -0500

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,86 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show story submit form on clicking story "submit" */
+
+function navShowStoryFormClick(evt) {
+  console.debug("navShowStoryFormClick", evt);
+  hidePageComponents();
+  $allStoriesList.show();
+  $submitForm.show();
+}
+
+$navSubmitStory.on("click", navShowStoryFormClick);
+
+/** Show fav stories on click on "favorites" */
+
+function navFavClick(evt) {
+  console.debug("navFavClick", evt);
+  hidePageComponents();
+  putFavsListOnPage();
+}
+
+$body.on("click", "#nav-favorites", navFavClick);
+
+/** Show My Stories on clicking "my stories" */
+
+function navMyStories(evt) {
+  console.debug("navMyStories", evt);
+  hidePageComponents();
+  putUserStoriesOnPage();
+  $myStories.show();
+}
+
+$body.on("click", "#nav-my-stories", navMyStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** Hide everything but profile on click on "profile" */
+
+function navProfileClick(evt) {
+  console.debug("navProfileClick", evt);
+  hidePageComponents();
+  $userProfile.show();
+}
+
+$navUserProfile.on("click", navProfileClick);
+
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+
+
+
+
+
+
